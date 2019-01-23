0b7c9c5fb68dc7cdd617cf0f436931221c9cc1a1
Michael Webb <trm@melonz.app>
2019-01-23 15:37:14 -0600

    Forgot to add this in the config

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ module.exports = {
         database: "fossticket",
         username: "root",
         password: "",
+        host: "localhost"
     },
     server: {
         port: 8080 // 8080 is default for FOSSTicket, 80/443 is for production
