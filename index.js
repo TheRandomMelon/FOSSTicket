29fccd8e1d7dda71b7c78b9ff34e157567a6603c
Michael Webb <trm@melonz.app>
2019-01-23 13:46:19 -0600

    Add some custom stuff instead of Bootstrap - it's good kinda

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+let port = 8080;
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
@@ -8,5 +9,5 @@ app.get('/', function(req, res) {
     res.render('pages/index');
 });
 
-app.listen(8080);
-console.log("Web server started!");
+app.listen(port);
+console.log(`Started the web server on port ${port}!`);
