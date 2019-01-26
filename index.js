d10b4e5fb4f754c6eaedfeaed9afc9c2ad46bccc
Michael Webb <trm@melonz.app>
2019-01-25 21:50:04 -0600

    more progress wooo

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 let config = require("./config.js");
+let express = require('express');
+let app = express();
 
-var express = require('express');
-var app = express();
-let port = 8080;
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
+let bcrypt = require('bcrypt');
+
+global.Sequelize = require('sequelize');
+global.sequelize = new Sequelize(config.mysql.database, config.mysql.username, config.mysql.password, {
+  	host: config.mysql.host,
+ 	dialect: 'mysql',
+  	operatorsAliases: false,
+
+  	pool: {
+    	max: 5,
+    	min: 0,
+    	acquire: 30000,
+    	idle: 10000
+  	}
+});
+
+// Models
+global.User = require("./models/User.js");
+
+sequelize.authenticate()
+  	.then(() => {
+    	console.log('Connection has been established to the MySQL database successfully.');
+  	})
+  	.catch(err => {
+		console.error('Unable to connect to the MySQL database:', err);
+		process.exit(1);
+  	});
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
     if (config.requiresSetup) {
@@ -15,9 +47,24 @@ app.get('/', function(req, res) {
     }
 });
 
+app.get('/dashboard', function(req, res) {
+    res.render("pages/dashboard");
+});
+
 app.post('/', function(req, res) {
     if (config.requiresSetup) {
-        console.error("Test");
+        let values = req.body;
+
+        if (values.password.length < 8) {
+            res.send("Password needs to be 8 characters or higher");
+        } else if (values.username.length < 1 || values.fullname.length < 1 || values.email.length < 1) {
+            res.send("Please define the username, full name and email fields.");
+        }
+
+        values.password = bcrypt.hashSync(values.password, 10);
+
+        User.create({ username: values.username, fullname: values.fullname, password: values.password, email: values.email, agent: 1, pfp: "/img/logo.png" });
+        res.redirect("/dashboard?firsttime=1");
     } else {
         res.send(`<html lang="en"><head>
         <meta charset="utf-8">
@@ -31,4 +78,4 @@ app.post('/', function(req, res) {
 });
 
 app.listen(config.server.port);
-console.log(`Started the web server on port ${config.server.port}!`);
+console.log(`Started the web server on port ${config.server.port}! Enjoy FOSSTicket!`);
---
+++ models/User.js
@@ -0,0 +1,11 @@
+module.exports = sequelize.define("user", {
+    id: {type: Sequelize.INTEGER, primaryKey: true},
+    username: Sequelize.STRING,
+    fullname: Sequelize.STRING,
+    password: Sequelize.STRING,
+    email: Sequelize.STRING,
+    agent: Sequelize.INTEGER,
+    pfp: Sequelize.STRING
+}, {
+    timestamps: false
+});
